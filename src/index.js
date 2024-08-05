ddedb4ee56a4af2aa3394418d1c44e9a71abebab
NataliaBadovska <n.badovska@gmail.com>
2024-08-05 16:26:12 +0200

    coments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from "./components/redux/store";
+// import { PersistGate } from 'redux-persist/integration/react'
+import { store} from "./components/redux/store";
 import  App  from 'components/App';
 import './index.css';
 
